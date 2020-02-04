3dd1ba555ae01d2840afa0895eaa1e8869049a87
%2 <%1>
2020-02-04 11:31:16 +0000

    Latest commit

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,6 +1,6 @@
 //Points to either mock API or our real API that's being served by Express
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://stark-stream-64952.herokuapp.com/';
 }
 
 function getQueryStringParameterByName (name, url) { 
